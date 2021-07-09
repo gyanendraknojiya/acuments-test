06390fadcb1f8ee7ff154fd651d5e4a5e18590f3
Gyanendra Kumar Knojiya <36006908+gyanendraknojiya@users.noreply.github.com>
2021-07-10 01:13:24 +0530

    Filter by launch date addd

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import MissionCard from "../components/MissionCard";
 import Header from "../components/Header";
-import { TextField } from "@material-ui/core";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
 
 const Homepage = () => {
   const data = useSelector((state) => state.data);
 
   const [previewData, setPreviewData] = useState([]);
-
   const [searchByName, setSearchByName] = useState(null);
+  const [filterByLaunchDate, setFilterByLaunchDate] = useState(null);// 1: last week, 2: last month, 3: last year
 
   useEffect(() => {
     setPreviewData(data);
@@ -17,6 +17,7 @@ const Homepage = () => {
 
   useEffect(() => {
     if (searchByName) {
+      setFilterByLaunchDate(null)
       let temp = data.filter((mission) =>
         mission.rocket.rocket_name.includes(searchByName)
       );
@@ -27,6 +28,42 @@ const Homepage = () => {
     }
   }, [searchByName, data]);
 
+  useEffect(() => {
+    if (filterByLaunchDate) {
+      setSearchByName(null)
+      if(filterByLaunchDate === 1){
+        let date = new Date()
+        const newDate = new Date(date.setDate(date.getDate() -7));
+        console.log(newDate.valueOf())
+        let temp = data.filter((mission) =>{
+          console.log(mission.launch_date_unix*1000)
+       return mission.launch_date_unix*1000 >=newDate.valueOf() &&mission.launch_date_unix*1000 <=Date.now()
+      });
+      setPreviewData([...temp]);
+      } else if (filterByLaunchDate === 2){
+        let date = new Date()
+        const newDate = new Date(date.setDate(date.getDate() -30));
+        console.log(newDate.valueOf())
+        let temp = data.filter((mission) =>{
+          console.log(mission.launch_date_unix*1000)
+       return mission.launch_date_unix*1000 >=newDate.valueOf() &&mission.launch_date_unix*1000 <=Date.now()
+      });
+      setPreviewData([...temp]);
+      } else if (filterByLaunchDate === 3){
+        let date = new Date()
+        const newDate = new Date(date.setDate(date.getDate() -365));
+        console.log(newDate.valueOf())
+        let temp = data.filter((mission) =>{
+          console.log(mission.launch_date_unix*1000)
+       return mission.launch_date_unix*1000 >=newDate.valueOf() &&mission.launch_date_unix*1000 <=Date.now()
+      });
+      setPreviewData([...temp]);
+      }
+    } else {
+      setPreviewData(data);
+    }
+  }, [filterByLaunchDate, data]);
+
   return (
     <>
       <Header />
@@ -35,18 +72,35 @@ const Homepage = () => {
         <div className="row mb-2 p-2 bg-light">
           <div className="col-md-4">
             <TextField
-              placeholder="Search by rocket name..."
+              value={searchByName}
+              label="Search by rocket name..."
+              className="w-100"
               onChange={(e) => setSearchByName(e.target.value)}
             />
           </div>
+          <div className="col-md-4" >
+          <FormControl className="w-100" >
+          <InputLabel id="filterByLaunchDate">Filter By Launch Date</InputLabel>
+          <Select
+          id="filterByLaunchDate"
+          value={filterByLaunchDate}
+          onChange={(e)=> setFilterByLaunchDate(e.target.value)}
+        >
+          <MenuItem value={1}>Last Week</MenuItem>
+          <MenuItem value={2}>Last Month</MenuItem>
+          <MenuItem value={3}>Last Year</MenuItem>
+        </Select>
+        </FormControl>
+          </div>
         </div>
-        {previewData?.length ? (
+        {searchByName && <h4 className="text-center py-3 text-white" >Search result for: {searchByName}</h4>}
+        {data?.length ? (
           <div className="row">
-            {previewData.map((mission, idx) => (
+            {previewData.length? previewData.map((mission, idx) => (
               <div className="col-md-4  my-2">
                 <MissionCard key={idx} {...mission} />
               </div>
-            ))}
+            )): <div className="col-12 text-center my-5 py-5" ><span className="rounded bg-light py-2 px-5 text-danger">No result found!</span> </div>}
           </div>
         ) : null}
       </div>
